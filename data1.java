4596195d19bf5e4eda85c236ff04d63ee5c90fe0
sai-reddy777 <csaikumarareddy996@gmail.com>
2024-09-27 21:43:37 +0530

    java

---
+++ data1.java
@@ -0,0 +1,22 @@
+class data1
+{
+public static void main(String[] args)
+{
+int a=10;
+if(a%2==0  && a%3==0)
+{
+System.out.println("good day");
+}
+else if(a%2==0)
+{
+System.out.println("my boy");
+}
+else if(a%3==0)
+{
+System.out.println("please welcome");
+}
+
+}
+}
+
+
---
+++ data2.java
@@ -0,0 +1,18 @@
+class data2
+{
+public static void main(String[] args)
+{
+int sum=0;
+int a=10;
+for(int i=0;i<=a;i++)
+{
+sum +=i;
+}
+System.out.println("Sum = "+a);
+
+}
+}
+
+
+
+
